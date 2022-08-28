34ae4a1a69868bb4959db7dded9d9bf3baebc48e
zxdfg114 <zxdfg114@gmail.com>
2022-08-28 19:27:42 +0900

    즉시 업데이트가 안되던 버그수정

--- JS/App copy.js
+++ JS/App copy.js
@@ -34,7 +34,7 @@ const input = document.querySelector('input');
 const date = new Date();
 const today = `${date.getFullYear()}-${String(date.getMonth()+1).padStart(2 , 0)}-${String(date.getDate()).padStart(2, 0)}`;
 const todoList = document.querySelector('#todo');
-const TODO = 'todo';
+const TODO = 'todo'
 
 let data = new Array;
 
@@ -46,8 +46,6 @@ function makeList(item){
   list.appendChild(span);
   todoList.appendChild(list);
   input.value = '';
-  updating();
-  deleting();
 }
 
 function savingData (e) {
@@ -65,59 +63,59 @@ submitBtn.addEventListener('click', savingData);
 const parsedData = JSON.parse(localStorage.getItem(TODO));
 
 if(localStorage.key(TODO) != null) {
-  for(let i = 0; i < parsedData.length; i++) {
+  for(let i of parsedData) {
     makeList(parsedData[i]);
     data = parsedData;
   }
 } 
 
 //===================================================================================
-function deleting(){
-  const deleteBtn = document.querySelectorAll('.fa-close');
+
+const deleteBtn = document.querySelectorAll('.fa-close');
 
 
-  for(let i =0; i < deleteBtn.length; i++) {
-    deleteBtn[i].addEventListener('click', function(){
-      (deleteBtn[i].parentNode).parentNode.remove();
-      data.splice(i, 1);
-      localStorage.setItem(TODO, JSON.stringify(data));
-    })
-  }
+for(let i =0; i < deleteBtn.length; i++) {
+  deleteBtn[i].addEventListener('click', function(){
+    (deleteBtn[i].parentNode).parentNode.remove();
+    data.splice(i, 1);
+    localStorage.setItem(TODO, JSON.stringify(data));
+  })
 }
 
 // =====================================================================================
-function updating(){
-  const updateBtn = document.querySelectorAll('.fa-pencil');
-  
-  for(let i =0 ; i < updateBtn.length; i++) {
-    updateBtn[i].addEventListener('click', function(){
-      let list = (updateBtn[i].parentElement).parentElement
-      const form = document.createElement('form');
-      const input = document.createElement('input');
-      const button = document.createElement('button');
-      const span = document.createElement('span');
-      input.value = `${data[i]}`;
-      button.innerText = `UPDATE`;
-      button.classList.add('update');
-      form.appendChild(input)
-      form.appendChild(button)
-      list.parentElement.insertBefore(form, list.nextSibling)
-      
-      const update = document.querySelector('.update');
-      update.addEventListener('click', function(e){
-        e.preventDefault();
-        console.log(input.value);
-        list.innerText = input.value;
-        list.appendChild(span)
-        span.innerHTML = `${today} <i class="fa fa-pencil fa-2x"> </i> <i class="fa fa-close fa-2x"></i>`;
-        data.splice(i, 1, input.value);
-        console.log(data)
-        localStorage.setItem(TODO, JSON.stringify(data));
-        form.remove();
-      })
+
+const updateBtn = document.querySelectorAll('.fa-pencil');
+
+for(let i =0 ; i < updateBtn.length; i++) {
+  updateBtn[i].addEventListener('click', function(){
+    let list = (updateBtn[i].parentElement).parentElement
+    const form = document.createElement('form');
+    const input = document.createElement('input');
+    const button = document.createElement('button');
+    const span = document.createElement('span');
+    input.value = `${data[i]}`;
+    button.innerText = `UPDATE`;
+    button.classList.add('update');
+    form.appendChild(input)
+    form.appendChild(button)
+    list.parentElement.insertBefore(form, list.nextSibling)
+    
+    const update = document.querySelector('.update');
+    update.addEventListener('click', function(e){
+      e.preventDefault();
+      console.log(input.value);
+      list.innerText = input.value;
+      list.appendChild(span)
+      span.innerHTML = `${today} <i class="fa fa-pencil fa-2x"> </i> <i class="fa fa-close fa-2x"></i>`;
+      data.splice(i, 1, input.value);
+      console.log(data)
+      localStorage.setItem(TODO, JSON.stringify(data));
+      form.remove();
     })
+  })
 }
-}
+
+
 
 
 
